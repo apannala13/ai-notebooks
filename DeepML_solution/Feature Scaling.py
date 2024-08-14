b3f92a6f3cc9fca241c4a09d9cd1795603f93871
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-08-13 22:36:28 -0700

    Create Feature Scaling.py

---
+++ DeepML_solution/Feature Scaling.py
@@ -0,0 +1,11 @@
+import numpy as np 
+def feature_scaling(data: np.ndarray) -> (np.ndarray, np.ndarray):
+    mean, std = np.mean(data, axis = 0), np.std(data, axis = 0)
+    standard_scaler = (data - mean) / std #standard scaling - guassian distribution, mean of 0 std of 1
+    
+    min_val, max_val = np.min(data, axis=0), np.max(data, axis=0) #transforms features to a specified range, 
+                                                                  #usually 0 to 1, by subtracting the minimum value and dividing by the range (max - min).
+    min_max_scaler = (data - min_val) / (max_val - min_val)
+    
+    return np.round(standard_scaler,4).tolist(), np.round(min_max_scaler,4).tolist()
+  
