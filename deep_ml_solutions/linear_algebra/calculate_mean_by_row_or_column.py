75164996d0f1d23e847c92f350a20080c7928f97
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-10-20 14:27:00 -0700

    add mean calc

---
+++ deep_ml_solutions/linear_algebra/calculate_mean_by_row_or_column.py
@@ -0,0 +1,6 @@
+import numpy as np
+def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
+	if mode == 'column':
+		return np.mean(matrix, axis=0)
+	else:
+		return np.mean(matrix, axis=1)
