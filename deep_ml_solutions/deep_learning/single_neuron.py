5bbed810e7dc68a5898391bc7241afc9603926ec
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-10-21 18:14:45 -0700

    fix format

--- deep_ml_solutions/deep_learning/single_neuron.py
+++ deep_ml_solutions/deep_learning/single_neuron.py
@@ -1,11 +1,17 @@
 import math
-import numpy as np 
+import numpy as np
 
-def sigmoid(z):
-	return 1 / (1 + np.exp(-z))
+def sigmoid(z: float) -> float:
+    return 1 / (1 + np.exp(-z))
 
-def single_neuron_model(features: list[list[float]], labels: list[int], weights: list[float], bias: float) -> (list[float], float):
-	preds = np.dot(features, weights) + bias
-	preds_sig = sigmoid(preds)
-	mse = np.mean(np.square(preds_sig - labels))
-	return np.round(preds_sig, 4).tolist(), np.round(mse, 4)
+def single_neuron_model(
+    features: list[list[float]], 
+    labels: list[int], 
+    weights: list[float], 
+    bias: float
+) -> (list[float], float):
+    
+    preds = np.dot(features, weights) + bias
+    preds_sig = sigmoid(preds)
+    mse = np.mean(np.square(preds_sig - labels))
+    return np.round(preds_sig, 4).tolist(), np.round(mse, 4)
