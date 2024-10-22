8db12928e5d8f379344087f51a22ea68e4f60d0d
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-10-21 18:17:09 -0700

    fix format

--- deep_ml_solutions/deep_learning/leakyreLU.py
+++ deep_ml_solutions/deep_learning/leakyreLU.py
@@ -1,5 +1,6 @@
+
 def leaky_relu(z: float, alpha: float = 0.01) -> float|int:
     if z <= 0.5:
-		return alpha * z 
-	else:
-		return max(0, z)
+        return alpha * z 
+    else:
+        return max(0, z)
