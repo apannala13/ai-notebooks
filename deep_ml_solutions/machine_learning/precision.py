f3a41e04b437812023bff5bee241379de63786a1
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-10-23 21:56:25 -0700

    add precision

---
+++ deep_ml_solutions/machine_learning/precision.py
@@ -0,0 +1,6 @@
+import numpy as np
+def precision(y_true, y_pred):
+	tp = np.sum((y_true == 1) & (y_pred == 1))
+	fp = np.sum((y_true == 0) & (y_pred == 1))
+	
+	return tp / (tp + fp) if (tp + fp) > 0 else 0.0
