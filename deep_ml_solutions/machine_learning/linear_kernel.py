3bee6ceb30e06256bc49cc79935a41421b8eac7b
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-11-04 10:30:08 -0800

    add svm linear kernel

--- deep_ml_solutions/machine_learning/linear_kernel.py
+++ deep_ml_solutions/machine_learning/linear_kernel.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 def kernel_function(x1, x2):
-	x1, x2 = np.array(x1), np.array(x2)
-	return np.dot(x1, x2)
+    # K(x₁, x₂) = x₁ ⋅ x₂ = ∑(i=1 to n) x₁ᵢ ⋅ x₂ᵢ
+    x1, x2 = np.array(x1), np.array(x2)
+    return np.dot(x1, x2)
