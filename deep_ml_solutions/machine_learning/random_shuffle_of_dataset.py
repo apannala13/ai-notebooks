9df53855b00b7cabca7b06bacd1557f80c8bf15f
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-11-05 18:27:09 -0800

    add random

--- deep_ml_solutions/machine_learning/random_shuffle_of_dataset.py
+++ deep_ml_solutions/machine_learning/random_shuffle_of_dataset.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def shuffle_data(X, y, seed=None):
-	if seed:
-		np.random.seed(seed) #set seed
-	indices = np.arange(X.shape[0])
-	np.random.shuffle(indices)
-	return X[indices], y[indices]
+    if seed is not None:
+        np.random.seed(seed)
+    indices = np.arange(X.shape[0])
+    np.random.shuffle(indices)
+    return X[indices], y[indices]
