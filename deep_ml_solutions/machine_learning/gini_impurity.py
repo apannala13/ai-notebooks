9f004d03d646c065bfa7fc9dab0326c095a10909
apannala13 <145242587+apannala13@users.noreply.github.com>
2024-11-05 18:06:55 -0800

    add gini

--- deep_ml_solutions/machine_learning/gini_impurity.py
+++ deep_ml_solutions/machine_learning/gini_impurity.py
@@ -1,16 +1,8 @@
 import numpy as np
 
 def gini_impurity(y):
-	#G = 1 - Σ(p_i^2), where p_i = count_of_class_i / num_elements_in_y
-	unique_classes, counts = np.unique(y, return_counts=True)
-	probas = counts / len(y)
-	gini = 1 - np.sum(np.square(probas))
-	return np.round(gini, 4)
-
-	
-	
-
-	
-	
-	
-	
+    #Gini Impurity formula: G = 1 - Σ(p_i^2), where p_i == probability of each unique class in y
+    unique_classes, counts = np.unique(y, return_counts=True)
+    probas = counts / len(y)  # Calculate probabilities for each class
+    gini = 1 - np.sum(np.square(probas))  # Sum of squares of probabilities
+    return np.round(gini, 4)
